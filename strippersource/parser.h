6a7afc4f1dfb7083ea53f6ad45abe2a626eeccc8
Asmodai Tabun <tabtemp@xs4all.nl>
2013-07-10 14:19:36 +0200

    build: 7b6b696412c5

---
+++ strippersource/parser.h
@@ -0,0 +1,86 @@
+/** vim: set ts=4 sw=4 et tw=99:
+ * 
+ * === Stripper for Metamod:Source ===
+ * Copyright (C) 2005-2009 David "BAILOPAN" Anderson
+ * No warranties of any kind.
+ * Based on the original concept of Stripper2 by botman
+ *
+ * License: see LICENSE.TXT
+ * ===================================
+ */
+#ifndef _INCLUDE_STRIP_PARSER_H
+#define _INCLUDE_STRIP_PARSER_H
+
+#include <sh_string.h>
+#include <sh_list.h>
+#include <sh_stack.h>
+#include "pcre.h"
+
+struct CACHEABLE
+{
+#if defined _DEBUG
+    CACHEABLE() : marked(true), last_alloc(0), last_free(0) { };
+    bool marked;
+    int last_alloc;
+    int last_free;
+#endif
+};
+
+struct parse_pair
+{
+    SourceHook::String key;
+    SourceHook::String val;
+    pcre *re;
+};
+
+struct ent_prop : public CACHEABLE
+{
+    SourceHook::String key;
+    SourceHook::String val;
+};
+
+struct replace_prop
+{
+    SourceHook::List<parse_pair *> match;
+    SourceHook::List<parse_pair *> to_replace;
+    SourceHook::List<parse_pair *> to_remove;
+    SourceHook::List<parse_pair *> to_insert;
+};
+
+class Stripper
+{
+public:
+    Stripper();
+    ~Stripper();
+public:
+    void SetEntityList(const char *ents);
+    void ApplyFileFilter(const char *file);
+    const char *ToString();
+private:
+    void RunRemoveFilter(SourceHook::List<parse_pair *> &filters);
+    void RunAddFilter(SourceHook::List<parse_pair *> &list);
+    void RunReplaceFilter(replace_prop &replace, SourceHook::List<parse_pair *> &props);
+    void Clear();
+    void _BuildPropList();
+private:
+    SourceHook::String *AllocString();
+    void FreeString(SourceHook::String *str);
+    ent_prop *AllocProp();
+    void FreeProp(ent_prop *prop);
+private:
+    SourceHook::CStack<SourceHook::String *> m_StringCache;
+    SourceHook::CStack<ent_prop *> m_PropCache;
+    void AppendToString(const char* buf, size_t len);
+private:
+    char* m_tostring;
+    size_t m_tostring_len;
+    size_t m_tostring_maxlen;
+    SourceHook::List<SourceHook::List<ent_prop *> *> m_props;
+    SourceHook::List<SourceHook::String *> m_lines;
+    bool m_resync;
+    pcre *brk_re;
+    pcre_extra *brk_re_extra;
+};
+
+#endif /* _INCLUDE_STRIP_PARSER_H */
+
